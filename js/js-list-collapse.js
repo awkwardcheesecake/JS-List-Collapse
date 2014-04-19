249099780196e666c1acf6826c2042c9b1ebb0d8
Elijah <awkwardcheesecake@Gmail.com>
2014-04-18 20:55:49 -0500

    Added a bunch of stuff
    Comments, code and examples in the demo file

--- js/js-list-collapse.js
+++ js/js-list-collapse.js
@@ -1,9 +1,12 @@
 $(document).ready(function(){
     $(".jslc-clickHeader").click(function () {
+		// Maybe kind of hacky, but eh, it works.
     	$(this).parent().children(".jslc-subBlock").toggle();
+		
+		// Boolean to check whether the sub block is visible or hidden
         var isActive = $(this).parent().children(".jslc-subBlock").is(":visible");
-        //$(".active").removeClass("active");
         
+		// Changes the expand symbol based on whether a click will expand or collapse the sub block
         if (!isActive) {
             $(this).children(".jslc-expandSymbol").text("+");
         }
