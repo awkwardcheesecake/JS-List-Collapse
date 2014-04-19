47badf534706ec597d432f386265ab11286cde41
Elijah <awkwardcheesecake@Gmail.com>
2014-04-18 21:42:26 -0500

    Changed some stuff
    made the click function a separate function so it will be easier to loop
    through and toggle stuff off, or something
    
    Mate is good stuff, but caffeine is not working today. It's been a long
    week.

--- js/js-list-collapse.js
+++ js/js-list-collapse.js
@@ -1,18 +1,23 @@
 $(document).ready(function(){
-    $(".jslc-clickHeader").click(function () {
-		// Maybe kind of hacky, but eh, it works.
-    	$(this).parent().children(".jslc-subBlock").toggle();
+	// add code to auto generate / set stuff here
+
+    $(".jslc-clickHeader").click(function() {
+		jslcToggleBlock(this)
+	});
+});
+
+function jslcToggleBlock(clickHeader) {
+	// Maybe kind of hacky, but eh, it works.
+    	$(clickHeader).parent().children(".jslc-subBlock").toggle();
 		
 		// Boolean to check whether the sub block is visible or hidden
-        var isActive = $(this).parent().children(".jslc-subBlock").is(":visible");
+        var isActive = $(clickHeader).parent().children(".jslc-subBlock").is(":visible");
         
 		// Changes the expand symbol based on whether a click will expand or collapse the sub block
         if (!isActive) {
-            $(this).children(".jslc-expandSymbol").text("+");
+            $(clickHeader).children(".jslc-expandSymbol").text("+");
         }
         else {
-        	$(this).children(".jslc-expandSymbol").text("-");
+        	$(clickHeader).children(".jslc-expandSymbol").text("-");
         }
-
-    });
-});
+}
