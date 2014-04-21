27b69d9f7579f05ed2c816585155b0df248edafd
Elijah <awkwardcheesecake@Gmail.com>
2014-04-20 21:39:28 -0500

    Update js-list-collapse.js
    Added brackets around the expand symbols so they look better

--- js/js-list-collapse.js
+++ js/js-list-collapse.js
@@ -2,12 +2,14 @@ $(document).ready(function(){
 	// Failsafe. If they have javascript disabled, nothing will happen.
 	// If they do have javascript, this will loop through and add the 
 	// expand symbol element and toggle the headers closed.
+	
 	$(".jslc-clickHeader").each(function() {
 		$(this).prepend('<span class="jslc-expandSymbol"></span> ');
 		jslcToggleBlock(this);
 	});
 
 	// Adds the click functionality
+	
     $(".jslc-clickHeader").click(function() {
 		jslcToggleBlock(this);
 	});
@@ -22,9 +24,9 @@ function jslcToggleBlock(clickHeader) {
         
 		// Changes the expand symbol based on whether a click will expand or collapse the sub block
         if (!isActive) {
-            $(clickHeader).children(".jslc-expandSymbol").text("+");
+            $(clickHeader).children(".jslc-expandSymbol").text("[+]");
         }
         else {
-        	$(clickHeader).children(".jslc-expandSymbol").text("-");
+        	$(clickHeader).children(".jslc-expandSymbol").text("[-]");
         }
 }
